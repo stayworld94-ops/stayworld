1d732d1f0db7196ed346f5aca9c60da0c348ac14
stayworld94-ops <stayworld94@gmail.com>
2025-08-15 22:32:34 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,34 +1,20 @@
-<!-- firebase-config.js -->
-<script type="module">
-  // CDN 모듈 버전 사용 (빌드 불필요)
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-  import {
-    getAuth,
-    GoogleAuthProvider,
-    signInWithPopup,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    updateProfile
-  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-  // (필요하면) import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-analytics.js";
+// firebase-config.js  (ESM)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import {
+  getAuth, GoogleAuthProvider, signInWithPopup,
+  signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile
+} from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 
-  // 👉 여기 당신이 받은 구성 그대로 붙이기
-  const firebaseConfig = {
-    apiKey: "AIzaSyCyb0pn2sFTEPkL0Q1ALwZaV2QILWyP_fk",
-    authDomain: "stayworld-2570c.firebaseapp.com",
-    projectId: "stayworld-2570c",
-    storageBucket: "stayworld-2570c.firebasestorage.app",
-    messagingSenderId: "272599681686",
-    appId: "1:272599681686:web:33f89b66f7ee6f6f0b50b7",
-    measurementId: "G-F8MXM3D7FJ"
-  };
+export const firebaseConfig = {
+  apiKey:        "YOUR_API_KEY",
+  authDomain:    "YOUR_PROJECT.firebaseapp.com",
+  projectId:     "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXXXXXXXXXX",
+  appId: "1:XXXXXXXXXXXX:web:YYYYYYYYYYYYYYYY",
+  measurementId: "G-XXXXXXXXXX"
+};
 
-  // Firebase 초기화
-  const app  = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  // const analytics = getAnalytics(app); // 선택사항
-
-  // 전역에서 쓸 수 있도록 window에 내보내기
-  window._swAuth = { auth, GoogleAuthProvider, signInWithPopup,
-                     signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile };
-</script>
+export const app  = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export { GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile };
