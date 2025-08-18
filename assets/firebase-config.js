d8f29d265744fd6464b549be0cd57733d48c374b
stayworld94-ops <stayworld94@gmail.com>
2025-08-18 06:01:46 -0500

    Create firebase-config.js

---
+++ assets/firebase-config.js
@@ -0,0 +1,11 @@
+// Replace with your actual Firebase config if different.
+// This can be safely exposed on the client for Firebase Web apps.
+export const firebaseConfig = {
+  apiKey: "AIzaSyCyb0pn2sFTEPkL0Q1ALwZaV2QILWyP_fk",
+  authDomain: "stayworld-2570c.firebaseapp.com",
+  projectId: "stayworld-2570c",
+  storageBucket: "stayworld-2570c.firebasestorage.app",
+  messagingSenderId: "272599681686",
+  appId: "1:272599681686:web:33f89b66f7ee6f6f0b50b7",
+  measurementId: "G-F8MXM3D7FJ"
+};
