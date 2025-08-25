8f14701eeb1ddd246e24469d1986dff0e8b89565
stayworld94-ops <stayworld94@gmail.com>
2025-08-25 14:39:17 -0500

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,10 +1,23 @@
-// Firebase 초기화 (원하는 값 넣기)
+// scripts/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth }       from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore }  from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage }    from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_APP.firebaseapp.com",
-  projectId: "YOUR_APP",
-  storageBucket: "YOUR_APP.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyCyb0pn2sFTEPkL0Q1ALwZaV2QILWyP_fk",
+  authDomain: "stayworld-2570c.firebaseapp.com",
+  projectId: "stayworld-2570c",
+  storageBucket: "stayworld-2570c.firebasestorage.app",
+  messagingSenderId: "272599681686",
+  appId: "1:272599681686:web:33f89b66f7ee6f6f0b50b7",
+  measurementId: "G-F8MXM3D7FJ"
 };
-firebase.initializeApp(firebaseConfig);
+
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+
+// export 해서 다른 페이지에서 공통으로 사용
+export const auth    = getAuth(app);
+export const db      = getFirestore(app);
+export const storage = getStorage(app);
