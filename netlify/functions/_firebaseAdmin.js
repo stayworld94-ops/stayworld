f79a961d2fd860b76bf96a2463b43c27de900226
stayworld94-ops <stayworld94@gmail.com>
2025-09-04 22:44:55 +0300

    Update _firebaseAdmin.js

--- netlify/functions/_firebaseAdmin.js
+++ netlify/functions/_firebaseAdmin.js
@@ -1,18 +1,11 @@
-// netlify/functions/_firebaseAdmin.js
+// _firebaseAdmin.js
 const admin = require('firebase-admin');
 
 if (!admin.apps.length) {
-  const projectId   = process.env.FIREBASE_PROJECT_ID;
-  const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
-  const privateKey  = (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n');
-
   admin.initializeApp({
-    credential: admin.credential.cert({ projectId, clientEmail, privateKey }),
-    // databaseURL: process.env.FIREBASE_DATABASE_URL, // (RealtimeDB 쓸 때)
+    // credential: admin.credential.applicationDefault(),
+    // databaseURL: process.env.FIREBASE_DATABASE_URL
   });
 }
 
-const db   = admin.firestore();
-const auth = admin.auth();
-
-module.exports = { admin, db, auth };
+module.exports = admin;
