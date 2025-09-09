0c38398687ad7a9f619da747eed9ebf32b4df4ae
stayworld94-ops <stayworld94@gmail.com>
2025-09-09 18:07:34 +0300

    Update _chatHelpers.js

--- netlify/functions/_chatHelpers.js
+++ netlify/functions/_chatHelpers.js
@@ -1,23 +1,18 @@
 // _chatHelpers.js
-// 공통 응답 헬퍼 함수 (CJS 방식: require/module.exports)
-
 function json(res, status, body) {
   res.statusCode = status;
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(body));
 }
 
-// 성공 응답
 function ok(res, body = { success: true }) {
   json(res, 200, body);
 }
 
-// 클라이언트 오류 응답
 function bad(res, msg = "Bad Request") {
   json(res, 400, { error: msg });
 }
 
-// 서버 오류 응답
 function fail(res, msg = "Internal Server Error") {
   json(res, 500, { error: msg });
 }
